6451ed81f62db7749b29a220966cba8776c6f03c
trang03011990 <86160198+trang03011990@users.noreply.github.com>
2022-01-10 18:13:13 +0700

    Update UserManagement.js

--- src/Pages/UserManagement/UserManagement.js
+++ src/Pages/UserManagement/UserManagement.js
@@ -32,7 +32,7 @@ export default function UserManagement(props) {
         setItemOffset(newOffset);
     };
     //END PAGINATION
-    
+
     //GHI DANH
     const [taiKhoanUser, setTaiKhoanUser] = useState({})
     const getUser = (i) => {
@@ -59,16 +59,16 @@ export default function UserManagement(props) {
         }
 
     }
-    
+
     //SEARCH VALUE
     const formik = useFormik({
         initialValues: { tuKhoa: '' },
     })
 
     useEffect(() => {
-        if(formik.values.tuKhoa!==""){
+        if (formik.values.tuKhoa !== "") {
             dispatch(getSearchList(formik.values.tuKhoa))
-        }else{
+        } else {
             dispatch(getUserList)
         }
     }, [formik.values.tuKhoa])
@@ -76,65 +76,96 @@ export default function UserManagement(props) {
 
     const renderUserList = (currentItems) => {
         return currentItems && currentItems.map((item, index) => {
-            return <tr>
-                <td className="align-middle">{itemOffset + index + 1}</td>
-                <td className="align-middle text-wrap">{item.taiKhoan}</td>
-                <td className="align-middle text-wrap">{item.maLoaiNguoiDung}</td>
-                <td className="align-middle text-wrap">{item.hoTen}</td>
-                <td className="align-middle text-break">{item.email}</td>
-                <td className="align-middle text-wrap">{item.soDt}</td>
-                <td>
-                    <button onClick={() => { getUser(item.taiKhoan)}}type="button" className="btn btn-success mx-1" data-toggle="modal" data-target="#userReg" >Ghi danh</button>
-                    <button onClick={() => { updateUser(item) }} className="btn btn-warning mx-1" data-toggle="modal" data-target="#userUpdate">Sửa</button>
-                    <button onClick={() => { deleteUser(item.taiKhoan) }} className="btn btn-danger mx-1" >Xóa</button>
+            return <tr style={{ width: '100%' }}>
+                <td className="align-middle text-break text-wrap" style={{ width: '5%' }}>{itemOffset + index + 1}</td>
+                <td className="align-middle text-break text-wrap" style={{ width: '13%' }} >{item.taiKhoan}</td>
+                <td className="align-middle text-break text-wrap" style={{ width: '5%' }} >{item.maLoaiNguoiDung}</td>
+                <td className="align-middle text-break text-wrap" style={{ width: '15%' }} >{item.hoTen}</td>
+                <td className="align-middle text-break text-wrap" style={{ width: '25%' }} >{item.email}</td>
+                <td className="align-middle text-break text-wrap" style={{ width: '10%' }} >{item.soDt}</td>
+                <td style={{ width: '27%' }}>
+                    <button onClick={() => { getUser(item.taiKhoan) }} type="button" className="btn mx-1 text-wrap" id="btnThem" data-toggle="modal" data-target="#userReg" >
+                        <span className='hide'>Ghi danh</span>
+                        <span className='unhide'>Reg</span>
+                    </button>
+                    <button onClick={() => { updateUser(item) }} className="btn btn-warning m-1 text-wrap" data-toggle="modal" data-target="#userUpdate">Sửa</button>
+                    <button onClick={() => { deleteUser(item.taiKhoan) }} className="btn btn-danger mx-1 text-wrap" >Xóa</button>
                 </td>
             </tr>
         })
 
     }
 
-    
+
     return (
         <div className="details container-fluid card text-center">
             {/* Header */}
-            <div className="card-header myCardHeader">
+            <div className="card-header myCardHeader container-fluid">
                 <div className="row">
-                    <div className="col-md-3 text-left ">
-                        <button className="btn btn-success" id="btnThem" data-toggle="modal" data-target="#myModal">Thêm người dùng</button>
-                    </div>
-                    <form onSubmit={formik.handleSubmit} className="col-md-6">
-                        <div className="input-group">
-                            <input name="tuKhoa"
-                                onBlur={formik.handleBlur}
-                                onChange={formik.handleChange}
-                                value={formik.values.tuKhoa} type="text" className="form-control" placeholder="Nhập vào tài khoản hoặc họ tên người dùng" id="searchName" />
-                            <div className="input-group-prepend">
-                                <button type="submit" className="input-group-text" id="btnTimNV"><i className="fa fa-search" /></button>
-                            </div>
-                        </div>
-                    </form>
+                    <div className='col-auto mr-auto row item'>
+                        <button id="dropdown-toggle menu-icon dropdownMenuButton2" data-toggle="dropdown" className="btn btn-light my-1 item unhide">
+                            <span id='menu-icon'>
+                                <i className="fa fa-list" />
+                            </span>
+                        </button>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                            <li><a className="dropdown-item" href="/admin/quanlynguoidung">Quản lý người dùng</a></li>
+                            <li><a className="dropdown-item" href="/admin/quanlykhoahoc">Quản lý khóa học</a></li>
+                        </ul>
+                        <button className="btn text-white m-1" id="btnThem" data-toggle="modal" data-target="#myModal">Thêm người dùng
+                        </button>
 
-                    <div className="col-md-3 text-right ">
-                        <LoginInfo/>
+                    </div>
+                    <div className='col-7 row ml-auto item'>
+                        <div className='col-7 p-1'>
+                            <form onSubmit={formik.handleSubmit} className="">
+                                {/* <div className="input-group py-1"> */}
+                                <input name="tuKhoa"
+                                    onBlur={formik.handleBlur}
+                                    onChange={formik.handleChange}
+                                    value={formik.values.tuKhoa} type="text" className="form-control" placeholder="Nhập vào tài khoản hoặc họ tên người dùng" id="searchName" />
+                                {/* </div> */}
+                            </form>
+
+                        </div>
+
+                        <div className="ml-auto">
+                            <LoginInfo />
+
+                        </div>
 
                     </div>
 
                 </div>
             </div>
             {/* Body */}
-            <div className="container">
-
+            <div className="container-fluid px-0">
                 <table className="table table-bordered table-hover myTable">
                     <thead className="text-dark bg-light">
-                        <tr>
-                            <th className="align-middle">STT</th>
-                            <th className="align-middle col-1">Tài khoản</th>
-                            <th className="align-middle col-1">Mật khẩu</th>
-                            <th className="align-middle col-2 ">Họ và tên</th>
-                            <th className="align-middle col-2 ">Email</th>
-                            <th className="align-middle col-2 ">Số điện thoại</th>
+                        <tr style={{ width: '100%' }}>
+                            <th className="align-middle text-break text-wrap" style={{ width: '7%' }} >
+                                <span className='hide'>STT</span>
+                                <span className='unhide'>STT</span>
+                            </th>
+                            <th className="align-middle text-break text-wrap" style={{ width: '13%' }}>
+                                <span className='hide'>Tài khoản</span>
+                                <span className='unhide'>TK</span>
+                            </th>
+                            <th className="align-middle text-break text-wrap" style={{ width: '7%' }} >
+                                <span className='hide'>Người dùng</span>
+                                <span className='unhide'>ND</span>
+                            </th>
+                            <th className="align-middle text-break text-wrap" style={{ width: '15%' }}>
+                                <span className='hide'>Họ và tên</span>
+                                <span className='unhide'>Họ Tên</span>
+                            </th>
+                            <th className="align-middle text-break text-wrap" style={{ width: '21%' }}>Email</th>
+                            <th className="align-middle text-break text-wrap" style={{ width: '12%' }}>
+                                <span className='hide'>Số điện thoại</span>
+                                <span className='unhide'>Điện thoại</span>
+                            </th>
 
-                            <th className="align-middle col-4"><em className="fa fa-cog" /></th>
+                            <th className="align-middle" style={{ width: '25%' }}><em className="fa fa-cog text-wrap" /></th>
                         </tr>
                     </thead>
                     <tbody id="tableDanhSach" >
@@ -145,11 +176,11 @@ export default function UserManagement(props) {
 
             {/* The Modal */}
             <UserModal />
-            <UserRegisterModal taiKhoanUser={taiKhoanUser}/>
+            <UserRegisterModal taiKhoanUser={taiKhoanUser} />
             <UserUpdateModal userUpdate={userUpdate} />
             <ReactPaginate
                 nextLabel="Sau >"
-                pageRangeDisplayed={5}
+                pageRangeDisplayed={3}
                 pageCount={pageCount}
                 onPageChange={handlePageClick}
                 previousLabel="< Trước"
